c800931c45a7824c884fdeb25d9bf66b57bebf97
matteobuscicchio <matteoezio@me.com>
2021-01-20 19:22:49 +0100

    costruito oggetto per generare menu navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,72 @@
 
 let sofboxApp = new Vue({
     el: '#sofbox',
-    data: {},
+    data: {
+        linkIndex: 0,
+        // sezione: NAVbar & logo
+        logo:'assets/img/logo.png',
+        navbar:[
+            {
+                style: 'active',
+                active: true,
+                section: 'Home'
+            },
+            {
+                style: 'active',
+                active: false,
+                section: 'About us'
+            },
+            {
+                style: 'active',
+                active: false,
+                section: 'Feature'
+            },
+            {
+                style: 'active',
+                active: false,
+                section: 'Testimonial'
+            },
+            {
+                style: 'active',
+                active: false,
+                section: 'Contact US'
+            },
+        ],
 
-    methods: {},
+        // sezione: Online Marketing
+        onlineMarketing: [
+            {
+                name: 'Online Marketing',
+                description: `It is a long establishment fact that a <br>reader will be distracted.`,
+                seo: 'SEO',
+                sem: 'SEM',
+                strategy: 'Website Strategy',
+                management: 'Social management',
+            },
+            {
+                name: 'Online Marketing',
+                description: `It is a long establishment fact that a <br>reader will be distracted.`,
+                seo: 'SEO',
+                sem: 'SEM',
+                strategy: 'Website Strategy',
+                management: 'Social management',
+            },
+            {
+                name: 'Online Marketing',
+                description: `It is a long establishment fact that a <br>reader will be distracted.`,
+                seo: 'SEO',
+                sem: 'SEM',
+                strategy: 'Website Strategy',
+                management: 'Social management',
+            }
+        ],
+    },
+
+    methods: {
+        activeLink: function(index){
+            this.linkIndex = index;
+            console.log(this.linkIndex);
+        },
+    },
     mounted(){}
 })
